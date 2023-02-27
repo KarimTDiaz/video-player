a39c5e13f35e390270263fc38fd1afbee8ac717b
Karim Tilouni Diaz <117732405+KarimTDiaz@users.noreply.github.com>
2023-02-27 13:20:13 +0100

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+// El styles lo importamos aquí, ya se carga después al compilar todo
+
+// import catImage from '../assets/images/cat.jpeg'
+import '../scss/styles.scss';
+import { sayHello } from './demo.js';
+
+sayHello();
